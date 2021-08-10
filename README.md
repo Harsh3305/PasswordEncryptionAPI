af30abbe6d0a81949d8c58a9ba3ef0b5b9d2e31d
Harsh Verma <harshverma3305@gmail.com>
2021-08-10 19:31:08 +0530

    Added README.md file

---
+++ README.md
@@ -0,0 +1,2 @@
+# Password Base Encryption API
+This API is created to encrypt given string using password base encryption.
---
+++ src/test/kotlin/com/example/passwordencryptionapi/PasswordEncryptionApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.passwordencryptionapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PasswordEncryptionApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
